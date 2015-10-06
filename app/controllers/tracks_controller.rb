ce2f1edcd4ac1469fce703beeb7977737dd8abe3
Anthony Riley <anth1y@gmail.com>
2015-10-05 19:14:51 -0700

    All things working

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -3,8 +3,8 @@ class TracksController < ApplicationController
     @track = Track.new
     @alltracks = Track.all
   end
-  def create 
-    Track.create(track_params)
+  def create
+    Track.create(track_params.merge(file: upload))
     redirect_to tracks_path, notice: "Track created"
   end
   def track_params
@@ -16,6 +16,7 @@ class TracksController < ApplicationController
     uploaded_io.original_filename), 'wb') do |f|
     f.write(uploaded_io.read)
     end
+    File.join('uploads', uploaded_io.original_filename)
   end
 end
 
